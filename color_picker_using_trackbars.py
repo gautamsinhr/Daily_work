094596883cef29860039f7c9221f774ccc9f2908
gautamsinhr <107615366+gautamsinhr@users.noreply.github.com>
2022-06-20 19:28:08 +0530

    Update color_picker_using_trackbars.py

--- color_picker_using_trackbars.py
+++ color_picker_using_trackbars.py
@@ -1,5 +1,3 @@
-#Create your own color picker -
-
 import cv2
 import numpy as np
 
@@ -9,16 +7,16 @@ def cross(x):
 img = np.zeros((300,512,3),np.uint8) #empty image
 cv2.namedWindow("Color Picker")
 
-#Creating switch for on and of the trackbars
-switch = '0 : OFF \n1 : ON'
+#creating switch for on and of the trackbars
+switch = '0 : off \n1 : on'
 cv2.createTrackbar(switch, 'Color Picker',0,1,cross)
 
-#Creating TrackBars For Adjusting Colors
+#creating trackBars 
 cv2.createTrackbar("R","Color Picker",0,255,cross)
 cv2.createTrackbar("G","Color Picker",0,255,cross)
 cv2.createTrackbar("B","Color Picker",0,255,cross)
 
-#Now creating logic to handle trackbars
+
 while True:
     cv2.imshow("Color Picker",img)
     k = cv2.waitKey(1) & 0xFF
